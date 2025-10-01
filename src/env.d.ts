db1da1682fc6003235335cd6fe59344c0851545b
Denis cazzulo <denis.cazzulo@m2sistemi.it>
2025-10-01 16:25:19 +0200

    Performance boost

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,3 +1,17 @@
 /// <reference types="astro/client" />
 
 export const SITE = "https://langamorepollenzo.it";
+
+declare global {
+	namespace astroHTML.JSX {
+		interface ImgHTMLAttributes {
+			fetchpriority?: "high" | "low" | "auto" | null;
+		}
+
+		interface LinkHTMLAttributes {
+			fetchpriority?: "high" | "low" | "auto" | null;
+		}
+	}
+}
+
+export {};
